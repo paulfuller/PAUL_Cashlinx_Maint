9a1759cf3d1cf56d000cb27b51621a49194d9b0d
mveldanda1 <mveldanda@cashamerica.com>
2012-06-11 13:32:59 -0500

    package structure for Describe Merchandise...

---
+++ src/Cashlinx/Support/Forms/ShopAdmin/Products/DescribeMerchandise/GunLookup.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace Support.Forms.ShopAdmin.Products.DescribeMerchandise
+{
+    partial class GunLookup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+
+        #endregion
+
+        private System.Windows.Forms.Label LblCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ReasonTypeDisplay;
+        private System.Windows.Forms.Label ReasonCodeDisplay;
+        private System.Windows.Forms.Label DescriptionDisplay;
+    }
+}

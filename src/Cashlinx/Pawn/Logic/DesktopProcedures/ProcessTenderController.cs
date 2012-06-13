339eb3171ccaa069d5ebd9b1fb91a86b332eb25d
srengarajan <srengarajan@casham.com>
2012-06-13 09:09:31 -0500

    Removed coupon amount from receiptdetail record's amount column and removed it from being inserted as a tender entry.

--- src/Cashlinx/Pawn/Logic/DesktopProcedures/ProcessTenderController.cs
+++ src/Cashlinx/Pawn/Logic/DesktopProcedures/ProcessTenderController.cs
@@ -9947,12 +9947,12 @@ namespace Pawn.Logic.DesktopProcedures
                     addlMdseItemRetPrice.Add(rItem.NegotiatedPrice.ToString());
                     addlcouponinfoType.Add(rItem.SaleType.ToString());
                     string opCode = Commons.GetInOpCode(TenderTypes.COUPON.ToString(), TenderTypes.CREDITCARD.ToString());
-                    if (GlobalDataAccessor.Instance.DesktopSession.TenderAmounts != null)
+                    /*if (GlobalDataAccessor.Instance.DesktopSession.TenderAmounts != null)
                         GlobalDataAccessor.Instance.DesktopSession.TenderAmounts.Add(rItem.CouponAmount.ToString());
                     if (GlobalDataAccessor.Instance.DesktopSession.TenderReferenceNumber != null)
                         GlobalDataAccessor.Instance.DesktopSession.TenderReferenceNumber.Add(rItem.CouponCode);
                     if (GlobalDataAccessor.Instance.DesktopSession.TenderTypes != null)
-                        GlobalDataAccessor.Instance.DesktopSession.TenderTypes.Add(opCode);
+                        GlobalDataAccessor.Instance.DesktopSession.TenderTypes.Add(opCode);*/
                     addlMdseCouponIcn.Add(rItem.Icn);
                     addlcouponCodes.Add(rItem.CouponCode);
                     addlcouponAmounts.Add(rItem.CouponAmount.ToString());
@@ -9964,12 +9964,12 @@ namespace Pawn.Logic.DesktopProcedures
             {
 
                 string opCode = Commons.GetInOpCode(TenderTypes.COUPON.ToString(), TenderTypes.CREDITCARD.ToString());
-                if (GlobalDataAccessor.Instance.DesktopSession.TenderAmounts != null)
+                /*if (GlobalDataAccessor.Instance.DesktopSession.TenderAmounts != null)
                     GlobalDataAccessor.Instance.DesktopSession.TenderAmounts.Add(GlobalDataAccessor.Instance.DesktopSession.ActiveRetail.CouponAmount.ToString());
                 if (GlobalDataAccessor.Instance.DesktopSession.TenderReferenceNumber != null)
                     GlobalDataAccessor.Instance.DesktopSession.TenderReferenceNumber.Add(GlobalDataAccessor.Instance.DesktopSession.ActiveRetail.CouponCode);
                 if (GlobalDataAccessor.Instance.DesktopSession.TenderTypes != null)
-                    GlobalDataAccessor.Instance.DesktopSession.TenderTypes.Add(opCode);
+                    GlobalDataAccessor.Instance.DesktopSession.TenderTypes.Add(opCode);*/
                 int i = 1;
                 totalCouponAmt += GlobalDataAccessor.Instance.DesktopSession.ActiveRetail.CouponAmount;
                 decimal couponAmtTotal = 0.0m;
@@ -10240,7 +10240,8 @@ namespace Pawn.Logic.DesktopProcedures
             refEvent.Add(ReceiptEventTypes.LAY.ToString());
 
             // ref amount is the active layaway amount
-            refAmount.Add((currentLayaway.DownPayment + couponTenderAmount).ToString());
+            //refAmount.Add((currentLayaway.DownPayment + couponTenderAmount).ToString());
+            refAmount.Add((currentLayaway.DownPayment).ToString());
 
             // ref store for sale is the store the receipt was printed at
             refStore.Add(GlobalDataAccessor.Instance.DesktopSession.CurrentSiteId.StoreNumber);

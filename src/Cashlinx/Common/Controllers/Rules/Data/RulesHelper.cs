01a277a22c19109a725d4fd906f4fe09c45da1e1
srengarajan <srengarajan@casham.com>
2012-06-20 16:06:50 -0500

    Fixes for #18,#19,#20

--- src/Cashlinx/Common/Controllers/Rules/Data/RulesHelper.cs
+++ src/Cashlinx/Common/Controllers/Rules/Data/RulesHelper.cs
@@ -679,7 +679,7 @@ namespace Common.Controllers.Rules.Data
                         continue;
                     }
                     var fndStateNumComps = from comp in components
-                                           where comp.Alias.IndexOf(site.State, StringComparison.OrdinalIgnoreCase) != -1
+                                           where comp.Alias.Equals(site.State, StringComparison.OrdinalIgnoreCase)
                                            select comp;
 
                     if (fndStateNumComps.Count() <= 0)
